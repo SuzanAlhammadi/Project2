25d831a9daf6b83a6d8e9535377c2b35b95162c9
Suzan <suzan@email.com>
2025-11-13 04:23:06 +0300

    add repeat calculation feature

--- home.py
+++ home.py
@@ -38,3 +38,51 @@ elif num2 < 0:
     print(num2, "is negative")
 else:
     print(num2, "is zero")
+
+
+while True:
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator: ")
+    num2 = float(input("Enter second number: "))
+
+    if operator == "+":
+        print(num1 + num2)
+    elif operator == "-":
+        print(num1 - num2)
+    elif operator == "*":
+        print(num1 * num2)
+    elif operator == "/":
+        print(num1 / num2)
+    elif operator == "**":
+        print(num1 ** num2)
+    else:
+        print("Invalid operator")
+
+
+    if num1 > num2:
+        print(num1, "greater than", num2)
+    elif num1 < num2:
+        print(num1, "less than", num2)
+    else:
+        print(num1, "equal to", num2)
+
+
+
+    if num1 > 0:
+        print(num1, "is positive")
+    elif num1 < 0:
+        print(num1, "is negative")
+    else:
+        print(num1, "is zero")
+
+    if num2 > 0:
+        print(num2, "is positive")
+    elif num2 < 0:
+        print(num2, "is negative")
+    else:
+        print(num2, "is zero")
+
+    again = input("Do you want to calculate again? (y/n): ")
+    if again != "y":
+        print("Thank you for using the calculator!")
+        break
