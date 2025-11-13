67cf331d7e245896c609ad4730e5a10287c5937a
Suzan <suzan@email.com>
2025-11-13 04:17:07 +0300

    add number sign check

--- home.py
+++ home.py
@@ -25,3 +25,16 @@ else:
     print(num1, "equal to", num2)
 
 
+if num1 > 0:
+    print(num1, "is positive")
+elif num1 < 0:
+    print(num1, "is negative")
+else:
+    print(num1, "is zero")
+
+if num2 > 0:
+    print(num2, "is positive")
+elif num2 < 0:
+    print(num2, "is negative")
+else:
+    print(num2, "is zero")
